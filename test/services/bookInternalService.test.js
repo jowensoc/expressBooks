8303091dfbb48b99b0be8c767e1d8ba65f080d14
John Owens <john.owens@opencastsoftware.com>
2025-01-03 12:18:33 +0000

    feat: add new search

--- test/services/bookInternalService.test.js
+++ test/services/bookInternalService.test.js
@@ -2,13 +2,47 @@ const assert = require("assert");
 const bookInternalService = require("../../src/services/bookInternalService")
 
 describe("book internal service", function() {
-   it("get books", function () {
-      let books = bookInternalService.getBooks();
-      assert(books.length > 0);
+
+   describe("get requests", function() {
+      it("- books", function () {
+         let books = bookInternalService.getBooks();
+         assert(books.length > 0);
+      });
+
+      it("- authors", function () {
+         let authors = bookInternalService.getAuthors();
+         assert(authors.length > 0);
+      });
    });
 
-   it("get authors", function () {
-      let authors = bookInternalService.getAuthors();
-      assert(authors.length > 0);
+   describe("search feature", function() {
+      it("- books by author. Should return results", function () {
+         let searchResults = bookInternalService.searchAuthors("Stephen", "King");
+         assert(searchResults.length > 0);
+      });
+
+      it("- books by author. Should not return any results", function () {
+         let searchResults = bookInternalService.searchAuthors("", "Test");
+         assert(searchResults.length === 0);
+      });
+
+      it("- books by title. Should return results", function () {
+         let searchResults = bookInternalService.searchBooks("Raft", "", "");
+         assert(searchResults.length > 0);
+         assert(searchResults[0].title === "Raft");
+      });
+
+      it("- books by title. Should not return any results", function () {
+         let searchResults = bookInternalService.searchBooks("test", "", "");
+         assert(searchResults.length === 0);
+      });
+
+      it("- books by title and author. Should return results", function () {
+         let searchResults = bookInternalService.searchBooks("Raft", "Stephen", "Baxter");
+         assert(searchResults.length > 0);
+         assert(searchResults[0].title === "Raft"
+                     && searchResults[0].authors[0] === "Stephen Baxter");
+      });
+
    });
 });
